32f31524da4cc8a0df941161c123f5031c2438ea
Aaron Stewart <aaron.stewart.contact@gmail.com>
2017-11-14 12:44:49 -0600

    Create topics feature, refactored posts to be nested in topics.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+
   resources :advertisments
   resources :questions
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
 
