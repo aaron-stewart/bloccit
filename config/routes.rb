ce99c005cdff8ee393866ea4588debd52d1a874c
Aaron Stewart <aaron.stewart.contact@gmail.com>
2017-11-28 14:47:12 -0600

    Generate Sessions controller, views, specs..

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
